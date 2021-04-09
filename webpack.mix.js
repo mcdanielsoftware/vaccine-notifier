e2cabb24a72d3da340e18a12cf1a8a185f3ca4eb
Rory McDaniel <hoyer801@gmail.com>
2021-04-08 20:12:28 -0400

    basic default homepage

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,7 +17,11 @@ mix.js('resources/js/app.js', 'public/js').vue()
         require('tailwindcss'),
     ])
     .webpackConfig(require('./webpack.config'));
-
 if (mix.inProduction()) {
     mix.version();
+} else {
+    mix.browserSync({
+        proxy: 'vaccinenotifier.lndo.site',
+        open: false,
+    });
 }
